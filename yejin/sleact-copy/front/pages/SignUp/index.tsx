3a735cdc7f20f6b8ec22dd09361b26e433cb94a7
twjin03 <twjindev@gmail.com>
2025-04-08 03:34:34 +0900

    feat: 회원가입 페이지 만들기
    참고) 앞 단계에서 누락된 부분 추가로 설치함
    "react-router": "^5.3.4",
    "react-router-dom": "^5.3.4"

--- yejin/sleact-copy/front/pages/SignUp/index.tsx
+++ yejin/sleact-copy/front/pages/SignUp/index.tsx
@@ -1,16 +1,34 @@
 import React, { useCallback, useState } from "react";
-import { Form, Label, Input, LinkContainer, Button, Header } from './styles';
+import { Form, Error,  Label, Input, LinkContainer, Button, Header } from './styles';
 
 const SignUp = () => {
-  const [email] = useState('');
-  const [nickname] = useState('');
-  const [password] = useState('');
-  const [passwordCheck] = useState('');
-  const onChangeEmail = useCallback(()=>{}, []);
-  const onChangeNickname = useCallback(()=>{}, []);
-  const onChangePassword = useCallback(()=>{}, []);
-  const onChangePasswordCheck = useCallback(()=>{}, []);
-  const onSubmit = useCallback(()=>{}, []);
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+
+  const [mismatchError, setMismatchError] = useState(false);
+  const onChangeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+  const onChangeNickname = useCallback((e) => {
+    setNickname(e.target.value);
+  }, []);
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+    setMismatchError(e.target.value !== passwordCheck);
+  }, [passwordCheck]);
+  const onChangePasswordCheck = useCallback((e) => {
+    setPasswordCheck(e.target.value);
+    setMismatchError(e.target.value !== password);
+  }, [password]);
+  const onSubmit = useCallback((e) => { // useCallback이 있어야 값 기억, 바뀔 때 함수 재생성 할 수 있음 
+    e.preventDefault();
+    console.log(email, nickname, password, passwordCheck);
+    if(!mismatchError){
+      console.log('서버로 회원가입하기')
+    }
+  }, [email, nickname, password, passwordCheck, mismatchError]);
 
   return (
     <div id="container">
@@ -47,14 +65,15 @@ const SignUp = () => {
           </div>
           {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-          {signUpError && <Error>{signUpError}</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
+          {/* {signUpError && <Error>{signUpError}</Error>} */}
+          {/* {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
-        <Link to="/login">로그인 하러가기</Link>
+        <a href="./login">로그인 하러가기</a>
+          {/* <Link to="/login">로그인 하러가기</Link> */}
       </LinkContainer>
     </div>
   );
