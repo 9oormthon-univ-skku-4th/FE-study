d50a2fd35990e19a390ea790dd864e0aef7f65cb
twjin03 <twjindev@gmail.com>
2025-04-08 14:12:46 +0900

    feat: 에러 메시지 표시하기
    성공/실패 시 state 변경
    화면에 결과 메시지 표시

--- yejin/sleact-copy/front/pages/SignUp/index.tsx
+++ yejin/sleact-copy/front/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from "react";
-import { Form, Error,  Label, Input, LinkContainer, Button, Header } from './styles';
+import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
 import useInput from "@hooks/useinput";
 import axios from "axios";
 
@@ -11,6 +11,9 @@ const SignUp = () => {
   const [passwordCheck, , setPasswordCheck] = useInput('');
 
   const [mismatchError, setMismatchError] = useState(false);
+  const [signUpError, setSignUpError] = useState('');
+
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
 
   const onChangePassword = useCallback((e) => {
     setPassword(e.target.value);
@@ -26,21 +29,25 @@ const SignUp = () => {
     e.preventDefault();
     console.log(email, nickname, password, passwordCheck);
 
-    if(!mismatchError){
+    if (!mismatchError) {
       console.log('서버로 회원가입하기')
+      setSignUpError('');
+      setSignUpSuccess(false);
       axios.post('http://localhost:3095/api/users', { // 포트 번호 유의 (3090이 3095'로' 보내는 것)
-        email, 
-        nickname, 
-        password, 
+        email,
+        nickname,
+        password,
       }) // 요청을 어디로? -> 백엔드 API리스트 참고 (모든 주소들 앞에는 API 생략되어있는거임)
         // Promise
         .then((response) => {
           console.log(response);
+          setSignUpSuccess(true);
         }) // 성공 
         .catch((error) => {
-          console.log(error.response);
+          console.log(error.response); // 콘솔에 data만 표시 ... 
+          setSignUpError(error.response.data); // 에러 메세지를 화면에 표시하기 위함 
         }) // 실패 error는 axios에 담겨 있음 
-        .finally(() => {}); // 항상 
+        .finally(() => { }); // 항상 
     }
 
   }, [email, nickname, password, passwordCheck, mismatchError]);
@@ -80,15 +87,15 @@ const SignUp = () => {
           </div>
           {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-          {/* {signUpError && <Error>{signUpError}</Error>} */}
-          {/* {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+          {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
         <a href="./login">로그인 하러가기</a>
-          {/* <Link to="/login">로그인 하러가기</Link> */}
+        {/* <Link to="/login">로그인 하러가기</Link> */}
       </LinkContainer>
     </div>
   );
