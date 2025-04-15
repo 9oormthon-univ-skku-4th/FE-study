87fd6349842b5ca3f7582d0aadc42e1e19762b35
twjin03 <twjindev@gmail.com>
2025-04-15 22:41:19 +0900

    feat: 챗박스 만들기

--- yejin/sleact-copy/front/components/ChatBox/index.tsx
+++ yejin/sleact-copy/front/components/ChatBox/index.tsx
@@ -1,23 +1,85 @@
 import { ChatArea, Form, MentionsTextarea, SendButton, Toolbox, EachMention } from '@components/ChatBox/styles';
+import { IUser } from '@typings/db';
 import React, { FC, useCallback, useEffect, useRef, VFC } from 'react';
 import { Mention, SuggestionDataItem } from 'react-mentions';
+import gravatar from 'gravatar';
+import autosize from 'autosize';
 
 interface Props {
   chat: string;
+  onSubmitForm: (e: any) => void;
+  onChangeChat: (e: any) => void;
+  placeholder: string;
+  data?: IUser[];
 }
 
-const ChatBox: VFC<Props> = ({ chat }) => {
+const ChatBox: VFC<Props> = ({ chat, onSubmitForm, onChangeChat, placeholder, data }) => {
 
-  const onSubmitForm = useCallback(() => {
-
+  const textareaRef = useRef<HTMLTextAreaElement>(null); // 태그에 직접 접근할 때 ref 씀 
+  useEffect(() => {
+    if (textareaRef.current) {
+      autosize(textareaRef.current);
+    }
   }, []);
+  //   const onSubmitForm = useCallback(() => {
+  // // DM 보내기 ? 채널 메시지 보내기 ? -> 여기서 하면 안됨, 구체적인 작업은 props로 올려주기 
+  //   }, []);
+  const onKeydownChat = useCallback(
+    (e) => {
+      if (!e.nativeEvent.isComposing && e.key === 'Enter') {
+        if (!e.shiftKey) {
+          e.preventDefault();
+          onSubmitForm(e);
+        }
+      }
+    },
+    [onSubmitForm],
+  );
+
+  const renderUserSuggestion: (
+    suggestion: SuggestionDataItem,
+    search: string,
+    highlightedDisplay: React.ReactNode,
+    index: number,
+    focused: boolean,
+  ) => React.ReactNode = useCallback(
+    (member, search, highlightedDisplay, index, focus) => {
+      if (!data) {
+        return null;
+      }
+      return (
+        <EachMention focus={focus}>
+          <img src={gravatar.url(data[index].email, { s: '20px', d: 'retro' })} alt={data[index].nickname} />
+          <span>{highlightedDisplay}</span>
+        </EachMention>
+      );
+    },
+    [data],
+  );
+
 
   return (
     <ChatArea>
       <Form onSubmit={onSubmitForm}>
         {/* <MentionsTextarea >
-          <textarea />
+          <input value={chat} onChange={onChangeChat}/>
         </MentionsTextarea> */}
+        <MentionsTextarea
+          id="editor-chat"
+          value={chat}
+          onChange={onChangeChat}
+          onKeyPress={onKeydownChat}
+          placeholder={placeholder}
+          inputRef={textareaRef}
+          forceSuggestionsAboveCursor
+        >
+          <Mention
+            appendSpaceOnAdd
+            trigger="@"
+            data={data?.map((v) => ({ id: v.id, display: v.nickname })) || []}
+            renderSuggestion={renderUserSuggestion}
+          />
+        </MentionsTextarea>
         <Toolbox>
           <SendButton
             className={
--- yejin/sleact-copy/front/pages/DirectMessage/index.tsx
+++ yejin/sleact-copy/front/pages/DirectMessage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Container, Header } from "./styles";
 import gravatar from 'gravatar'
 import useSWR from "swr";
@@ -6,25 +6,39 @@ import { useParams } from "react-router";
 import fetcher from "@utils/fetcher";
 import ChatBox from "@components/ChatBox";
 import ChatList from "@components/ChatList";
+import useInput from "@hooks/useInput";
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher); // 상대방 데이터
-  const { data: myData } = useSWR('/api/users', fetcher); 
+  const { data: myData } = useSWR('/api/users', fetcher);
+  const [chat, onChangeChat] = useInput('');
+
+  // hooks는 return보다 올려서 
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    console.log('submit');
+  }, []);
 
   // 로딩 중 or error 일 때 화면 띄우지 않기 
-  if (!userData || !myData){
-    return null; 
+  if (!userData || !myData) {
+    return null;
   }
   return (
     <Container>
       <Header>
-      <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
-      <span>{userData.nickname}</span> 
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
       </Header>
-      <ChatList/>
-      <ChatBox chat=" "/>
-  
+      <ChatList />
+      <ChatBox
+        onSubmitForm={onSubmitForm}
+        chat={chat}
+        onChangeChat={onChangeChat}
+        placeholder={`Message ${userData.nickname}`}
+        data={[]}
+      />
+
 
 
     </Container>
