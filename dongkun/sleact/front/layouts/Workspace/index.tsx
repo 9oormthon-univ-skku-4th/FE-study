23e733363f2bdb956a364f77142e371f9e443f14
GunderGie <rjs94951@gmail.com>
2025-04-22 23:26:10 +0900

    feat: onCreateWorkspace post요청 구현

--- dongkun/sleact/front/layouts/Workspace/index.tsx
+++ dongkun/sleact/front/layouts/Workspace/index.tsx
@@ -53,7 +53,37 @@ const Workspace: FC = ({ children }) => {
     setShowCreateWorkspaceModal((prev) => !prev);
   }, []);
 
-  const onCreateWorkspace = useCallback(() => {}, []);
+  const onCreateWorkspace = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!newWorkspace || !newWorkspace.trim()) {
+        return;
+      }
+      if (!newUrl || !newUrl.trim()) {
+        return;   // trim은 space만 입력 방지
+      } 
+      
+      axios
+        .post(
+          'http://localhost:3095/api/workspaces',
+          {
+            workspace: newWorkspace,
+            url: newUrl,
+          },
+          { withCredentials: true },
+        )
+        .then(() => {
+          mutate();
+          setShowCreateWorkspaceModal(false);
+          setNewWorkspace('');
+          setNewUrl('');
+        })
+        .catch((error) => {
+          console.dir(error);
+        });
+    },
+    [newWorkspace, newUrl],
+  );
 
   const onCloseModal = useCallback(() => {
     setShowCreateWorkspaceModal(false);
@@ -118,8 +148,8 @@ const Workspace: FC = ({ children }) => {
             <Input id="workspace" value={newWorkspace} onChange={onChangeNewWorkspace} />
           </Label>
           <Label id="workspace-url-label">
-            <span>워크스페이스 이름</span>
-            <Input id="workspace" value={newUrl} onChange={onChangeNewUrl} />
+            <span>워크스페이스 URL</span>
+            <Input id="workspace-url" value={newUrl} onChange={onChangeNewUrl} />
           </Label>
           <Button type="submit">생성하기</Button>
         </form>
