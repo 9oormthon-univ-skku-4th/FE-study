62a54a98b9b1c9ed68f2f18b42b10f14ae7217f2
twjin03 <twjindev@gmail.com>
2025-04-29 22:17:08 +0900

    feat: 커스텀 스크롤바 구현
    react-custom-scrollbars-2

--- yejin/sleact-copy/front/components/ChatList/index.tsx
+++ yejin/sleact-copy/front/components/ChatList/index.tsx
@@ -1,18 +1,25 @@
-import React, { VFC } from "react";
+import React, { useCallback, useReducer, useRef, VFC } from "react";
 import { ChatZone } from "./styles";
 import { IDM } from "@typings/db";
 import Chat from "@components/Chat";
+import Scrollbars from "react-custom-scrollbars-2";
 
 interface Props {
   chatData?: IDM[];
 }
 
 const ChatList: VFC<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => { }, []);
+
   return (
     <ChatZone>
-      {chatData?.map((chat) => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
+
     </ChatZone>
   );
 };
