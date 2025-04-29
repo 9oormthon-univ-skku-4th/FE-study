65e68ddd9494e58509112c83ffe0aa969cd988a2
Durumi <ps2601@naver.com>
2025-04-29 21:40:50 +0900

    feat: 채널 만들기

--- junhyeong/week1/front/hooks/useInput.ts
+++ junhyeong/week1/front/hooks/useInput.ts
@@ -1,11 +1,13 @@
-import { Dispatch, SetStateAction, useCallback, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState, ChangeEvent } from 'react';
 
-const useInput = <T = any>(initialData: T): [T, (e: any) => void, Dispatch<SetStateAction<T>>] => {
-    const [value, setValue] = useState(initialData);
-    const handler = useCallback((e) => {
-        setValue(e.target.value);
-    }, []);
-    return [value, handler, setValue];
+type ReturnTypes<T> = [T, (e: ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>];
+
+const useInput = <T>(initialData: T): ReturnTypes<T> => {
+  const [value, setValue] = useState(initialData);
+  const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setValue((e.target.value as unknown) as T);
+  }, []);
+  return [value, handler, setValue];
 };
 
 export default useInput;
