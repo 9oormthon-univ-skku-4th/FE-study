7b50833fc402e707a153be8410fb27294996eb75
twjin03 <twjindev@gmail.com>
2025-04-30 02:36:04 +0900

    feat: 정규표현식으로 문자열 변환

--- yejin/sleact-copy/front/components/Chat/index.tsx
+++ yejin/sleact-copy/front/components/Chat/index.tsx
@@ -1,28 +1,52 @@
-import React, { VFC } from "react";
+import React, { memo, useMemo, VFC } from "react";
 import { ChatWrapper } from "./styles";
 import { IDM } from "@typings/db";
 import gravatar from 'gravatar';
 import dayjs from "dayjs";
+import regexifyString from "regexify-string";
+import { Link, useParams } from "react-router-dom";
+import { channel } from "diagnostics_channel";
 
 interface Props {
   data: IDM;
 }
 
 const Chat: VFC<Props> = ({ data }) => {
+  const {workspace} = useParams<{workspace: string; channel: string}>();
   const user = data.Sender;
+
+  // @[제로초](7)
+  // \d 숫자      +는 1개 이상 ?는 0개나 1가 *은 0개 이상    g는 모두 찾기 
+  const result = regexifyString({
+    input: data.content,
+    pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+    decorator(match, index) {
+      const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+      // id만 찾아서 id에 걸리는 게 있으면 
+      if (arr) {
+        return (
+          <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`} >
+            @{arr[1]}
+          </Link>
+        )
+      }
+      return <br key={index} />; // 줄바꿈 
+    },
+  });
+
   return (
     <ChatWrapper>
-    <div className="chat-img">
-      <img src={gravatar.url(user.email, { s: '36px', d: 'retro' })} alt={user.nickname} />
-    </div>
-    <div className="chat-text">
-      <div className="chat-user">
-        <b>{user.nickname}</b>
-        <span>{dayjs(data.createdAt).format('h:mm A')}</span>
+      <div className="chat-img">
+        <img src={gravatar.url(user.email, { s: '36px', d: 'retro' })} alt={user.nickname} />
       </div>
-      <p>{data.content}</p>
-    </div>
-  </ChatWrapper>
+      <div className="chat-text">
+        <div className="chat-user">
+          <b>{user.nickname}</b>
+          <span>{dayjs(data.createdAt).format('h:mm A')}</span>
+        </div>
+        <p>{result}</p>
+      </div>
+    </ChatWrapper>
   );
 };
 
