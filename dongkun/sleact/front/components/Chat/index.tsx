2d55b185f05b96db63aa994be4a8ef9749bc8adf
GunderGie <rjs94951@gmail.com>
2025-04-30 13:48:53 +0900

    feat: 멘션 결과 하이라이팅&링크연결

--- dongkun/sleact/front/components/Chat/index.tsx
+++ dongkun/sleact/front/components/Chat/index.tsx
@@ -1,14 +1,35 @@
-import React, { VFC } from 'react';
+import React, { useMemo, VFC } from 'react';
 import { ChatWrapper } from './styles';
 import gravatar from 'gravatar';
 import { IDM } from '@typings/db';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link, useParams } from 'react-router-dom';
 
 interface Props {
   data: IDM;
 }
 const Chat: VFC<Props> = ({ data }) => {
   const user = data.Sender;
+  const { workspace } = useParams<{ workspace: string; channel: string }>();
+
+  // @[닉네임](7)
+  const result = useMemo(()=> regexifyString({
+    input: data.content,
+    pattern: /@\[(.+?)\]\((\d+?)\)|\n/g,
+    decorator(match, index) {
+      const arr:string[]|null = match.match(/@\[(.+?)\]\((\d+?)\)/)!;
+      if (arr) {
+        return (
+          <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+            @{arr[1]}
+          </Link>
+        );
+      }
+      return <br key={index} />;
+    },
+  }), [data.content]);
+
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -19,7 +40,7 @@ const Chat: VFC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
